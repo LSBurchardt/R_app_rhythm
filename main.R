b9741978e6cdfe7d460e219b04f317ab34d01e1c
LSBurchardt <l.s.burchardt@gmx.de>
2021-11-08 14:08:54 +0100

    initial commit

---
+++ main.R
@@ -0,0 +1,38 @@
+######################################################################################
+# Rhythm Analysis Methods in R 
+# Transfering script from Matlab to R
+# author: Dr. Lara S. Burchardt
+# 
+# date: 08.11.2021, version: 1.0
+# 
+# main script
+######################################################################################
+
+## 00a: load packages-----------
+
+
+## 00b: load example data ------
+
+
+## 01: Set path ----------------
+# batch processing, choose whole folder to conduct analysis
+
+
+## 02: Binary ------------------
+# transform time labels to binary sequence
+
+
+## 03: Fourier anlysis ---------
+# calculate FFT over binary sequence to calculate best fitting beat
+
+
+## 04: IOI analysis-------------
+# calculate IOI rhythm over original sequence to calculate best fitting beat
+
+
+## 05: ugof---------------------
+# calculate goodness-of-fit for IOI analysis and Fourier analysis
+
+
+## 06: plotting ----------------
+# possible plot: histogram, scatter plot, recurrence plot
