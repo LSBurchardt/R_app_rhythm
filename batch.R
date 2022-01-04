2016701f1794767f709f9f8206db89955f33882b
Lara S. Burchardt <l.s.burchardt@gmx.de>
2022-01-04 09:59:54 +0100

    batch processing, start

---
+++ batch.R
@@ -0,0 +1,23 @@
+######################################################################################
+# Rhythm Analysis Methods in R 
+# Transfering script from Matlab to R
+# author: Dr. Lara S. Burchardt
+# 
+# date: 08.11.2021, version: 1.0
+# 
+# batch processing
+######################################################################################
+
+batch_processing <- function(data){
+
+## 00: load packages-----------
+
+library(tidyverse)
+
+## 01: establish vector with all data, that is to be processed
+  
+  #list.files
+  
+  
+  
+}
